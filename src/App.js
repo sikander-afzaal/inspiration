4a7fb68011f2f533d7a589e20300cb31cd7295d1
sikander-afzaal <72333576+sikander-afzaal@users.noreply.github.com>
2022-03-22 17:15:22 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Header from "./Components/Header";
+import Home from "./Pages/Home";
+import Hem from "./Pages/Hem";
+import Kontkat from "./Pages/Kontkat";
+import Projekt from "./Pages/Projekt";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Hem />} />
+          <Route path="/kontakt" element={<Kontkat />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
